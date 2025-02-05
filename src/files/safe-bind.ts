5539911dc11f951d2fdab78bde7ba4035a3e907b
Dárius Bokor <info@weroro.sk>
2025-02-05 22:49:00 +0100

    update

--- src/files/safe-bind.ts
+++ src/files/safe-bind.ts
@@ -1,12 +1,9 @@
 interface SafeBind {
-    <T extends ((...argArray: Parameters<T>) => ReturnType<T>)>
-    (fn: T): T;
+    <T extends ((...argArray: Parameters<T>) => ReturnType<T>)>(fn: T): T;
 
-    <T extends ((...argArray: Parameters<T>) => ReturnType<T>), U>
-    (fn: T, thisArg: object): T;
+    <T extends ((...argArray: Parameters<T>) => ReturnType<T>)>(fn: T, thisArg: object): T;
 
-    <T extends ((...argArray: Parameters<T>) => ReturnType<T>)>
-    (fn: T, thisArg?: object | null, ...argArray: Parameters<T>): () => ReturnType<T>;
+    <T extends ((...argArray: Parameters<T>) => ReturnType<T>)>(fn: T, thisArg: object | null, ...argArray: Parameters<T>): () => ReturnType<T>;
 }
 
 /**
